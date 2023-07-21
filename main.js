837aa9a16bb11eeaba7e01ec232b04e9b1f543c8
kitomisat <138214042+kitomisat@users.noreply.github.com>
2023-07-21 17:55:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 tour_list = [
   {
     "name": "Station", // 星ヶ丘駅
-    "lat": 35.16269945778448,
-    "lon": 136.98522053584585,
+    "lat": 35.351535,
+    "lon": 137.1097135,
     "distance": 0,
     "visible": false,
   },
